218d912abbb6bc67a3489e950ec1857156940f26
Mg-Ye-Yint <tai31238@gmail.com>
2024-04-06 23:11:13 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Adder from "@/components/Adder";
 import Header from "@/components/Header";
 import prisma from "@/helper/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const notes = await prisma.note.findMany({ orderBy: { createdAt: "asc" } });
 
